15d5fc2c942d830d5412cd04dab9ae9431575dc3
Gabriel Silva <107951139+GBLSLVA@users.noreply.github.com>
2026-09-23 20:39:35 -0300

    refactor: usar GoalRepository nos insights

--- server/services/insight-service.mjs
+++ server/services/insight-service.mjs
@@ -9,8 +9,9 @@ import {
 } from './finance-service-utils.mjs';
 
 export class InsightService {
-  constructor({entries,debts,incomes,budgets}) {
+  constructor({entries,goals,debts,incomes,budgets}) {
     this.entries = entries;
+    this.goals = goals;
     this.debts = debts;
     this.incomes = incomes;
     this.budgets = budgets;
@@ -24,7 +25,7 @@ export class InsightService {
     const [transactions, debts, goals, incomes, budgets] = await Promise.all([
       this.entries.list(user,'transactions'),
       this.debts.list(user),
-      this.entries.list(user,'goals'),
+      this.goals.list(user),
       this.incomes.list(user),
       this.budgets.list(user,monthKey),
     ]);
